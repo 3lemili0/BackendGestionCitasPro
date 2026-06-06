2ebf05a87fcfda5b54329f9c484182495d7b91c5
3lemili0 <jpaezm@unicartagena.edu.co>
2026-06-06 15:42:23 -0500

    Se corriegieron controladores y modelos del backend para despliegue

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,22 +1,18 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const conectarDB = async () => {
     try {
-        const connection = await mongoose.connect(process.env.MONGO_URI);
+        // Usamos la variable de entorno MONGO_URI
+        const db = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
 
-        const url = connection.connection.host;
-        const dbName = connection.connection.name;
-        console.log(`
-        ##################################################
-        MongoDB Conectado Exitosamente.
-        Servidor: ${url}
-        Base de Datos: ${dbName}
-        ##################################################
-        `);
-
+        const url = `${db.connection.host}:${db.connection.port}`;
+        console.log(`MongoDB conectado en: ${url}`);
     } catch (error) {
-        console.error("Error al conectar MongoDB:", error);
-        process.exit(1);
+        console.error(`Error al conectar a MongoDB: ${error.message}`);
+        process.exit(1); // Detiene la aplicación si no se puede conectar a la BD
     }
 };
 
